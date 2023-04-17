84f0b4bc7e76045f35da17cdb8defa74d1c42551
Paco <pacors88@gmail.com>
2023-04-18 00:20:28 +0200

    first commit

---
+++ 1-dart-intro/01_hello_world.dart
@@ -0,0 +1,14 @@
+void main() {
+  // var myName = 'Paco';
+  // String myName = 'Paco';
+  
+  // final myName = 'Paco';
+  // late final myName;
+  // myName = 'Paco';
+  
+  // const myName = 'Paco';
+  
+  // print('Hola $myName');
+  // print('Hola ${myName.toLowerCase()}');
+  print('Hola ${1 + 1}');
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# FH Course Flutter 2023

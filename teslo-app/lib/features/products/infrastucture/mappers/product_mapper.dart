a40dde1eadab309000c1dddfb7b06bc7d6f987c0
Paco <pacors88@gmail.com>
2023-07-09 21:50:16 +0200

    end section 28

--- teslo-app/lib/features/products/infrastucture/mappers/product_mapper.dart
+++ teslo-app/lib/features/products/infrastucture/mappers/product_mapper.dart
@@ -6,7 +6,7 @@ class ProductMapper {
   static jsonToEntity(Map<String, dynamic> json) => Product(
     id: json['id'],
     title: json['title'],
-    price: double.parse(json['price']),
+    price: double.parse(json['price'].toString()),
     description: json['description'],
     slug: json['slug'],
     stock: json['stock'],
@@ -15,7 +15,7 @@ class ProductMapper {
     tags: List<String>.from(json['tags'].map((tag) => tag)),
     images: List<String>.from(
       json['images'].map(
-        (String image) => image.startsWith('http')
+        (image) => image.startsWith('http')
           ? image
           : '${Environment.apiUrl}/files/product/$image'
       )
